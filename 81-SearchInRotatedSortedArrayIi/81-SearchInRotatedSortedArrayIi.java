cd868e67d352ba89ace9f4298e299d8f57db722b
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2025-08-02 22:39:34 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.java
@@ -0,0 +1,36 @@
+// Last updated: 8/2/2025, 10:39:19 PM
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int start =  0;
+        int end =  arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            }
+
+            if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                start++;
+                end--;
+            }
+            else if (arr[start] <= arr[mid]) {
+                if (arr[start] <= target && target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            else {
+                if (arr[mid] < target && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return false;  
+    }
+}
