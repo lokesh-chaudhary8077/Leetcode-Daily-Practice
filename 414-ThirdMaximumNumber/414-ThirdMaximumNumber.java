69a9b74ae73213443d7a5e60605fa29e725b599b
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2025-09-17 22:15:55 +0530

    File updated at 9/17/2025, 10:15:51 PM

--- 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -1,27 +1,36 @@
-// Last updated: 9/5/2025, 11:54:25 PM
+// Last updated: 9/17/2025, 10:15:51 PM
 class Solution {
-    public int thirdMax(int[] nums) {
-        long v1 = Long.MIN_VALUE; 
-        long v2 = Long.MIN_VALUE;
-        long v3 = Long.MIN_VALUE;
-        for(int i = 0;i < nums.length;i++){
-            if(nums[i] == v1 || nums[i] == v2 || nums[i] == v3){
-                continue;
-            }
-            if(nums[i] > v1){
-                v3 = v2;
-                v2 = v1;
-                v1 = nums[i];
-            }
-            else if(nums[i] > v2){
-                v3 = v2;
-                v2 = nums[i];
-            }
-            else if(nums[i] > v3) {
-                v3 = nums[i];
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int pivot = -1;
+        for(int i = n - 2;i >= 0;i--){
+            if(nums[i] < nums[i + 1]){
+                pivot = i;
+                break;
             }
         }
-        if(v3 == Long.MIN_VALUE) return (int)v1;
-        else return (int)v3;
+        if(pivot == -1){
+            reverse(nums,0,n-1);
+            return;
+        }
+        for(int i = n -1;i> 0;i--){
+            if(nums[i] > nums[pivot]){
+                swap(nums,i,pivot);
+                break;
+            }
+        }
+        reverse(nums,pivot+1,n-1);
+    }
+    public void reverse(int nums[],int i,int j){
+        while(i < j){
+            swap(nums,i,j);
+            i++;
+            j--;
+        }
+    }
+    public void swap(int nums[],int i ,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
