e682025a94b0bbd0e79d94384be3bde555b29288
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2025-09-18 22:50:27 +0530

    File updated at 9/18/2025, 10:50:23 PM

--- 2965-FindMissingandRepeatedValues/2965-FindMissingandRepeatedValues.java
+++ 2965-FindMissingandRepeatedValues/2965-FindMissingandRepeatedValues.java
@@ -1,24 +1,24 @@
-// Last updated: 8/3/2025, 10:06:51 PM
+// Last updated: 9/18/2025, 10:50:23 PM
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-       HashSet<Integer> set = new HashSet<>();
-       int n = grid.length;
-       int sq =  n * n;
-       int ans[] = new int[2];
-       int currSum = 0;
-       for (int i = 0;i < n; i++){
-            for(int j = 0; j < n ;j++){
-                if(set.contains(grid[i][j])){
-                    ans[0] = grid[i][j];
-                }
-                else {
-                    set.add(grid[i][j]);
-                    currSum += grid[i][j];
-                }
+        int n = grid.length;
+        int maxval = n*n;
+        int count[] = new int[maxval+1];
+        int repeat = -1;
+        int missing =  -1;
+        for(int i[] : grid){
+            for(int j : i){
+                count[j]++;
             }
         }
-        int totalSum = sq *(sq + 1)/2;
-        ans[1] = totalSum - currSum;
-        return ans;
+        for(int i = 1;i <= maxval;i++){
+            if(count[i] == 2){
+                repeat = i;
+            }
+            if(count[i] == 0){
+                missing = i;
+            }
+        }
+        return new int[]{repeat, missing};
     }
 }
