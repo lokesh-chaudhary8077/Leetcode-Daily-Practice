3b55677c44b34fee42983459dbac8a2146306838
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2025-11-11 22:37:15 +0530

    File updated at 11/11/2025, 10:37:08 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.java
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -1,18 +1,18 @@
-// Last updated: 8/11/2025, 9:46:51 PM
+// Last updated: 11/11/2025, 10:37:08 PM
 class Solution {
-    public int searchInsert(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length - 1;
-        while(start <= end){
-            int mid = (start + end)/2;
-            if(arr[mid] == target){
+    public int searchInsert(int[] nums, int target) {
+        int si = 0;
+        int ei = nums.length - 1;
+        while(si <= ei){
+            int mid = (si + ei)/2;
+            if(nums[mid] == target){
                 return mid;
+            }else if(nums[mid] > target){
+                ei = mid - 1;
+            }else {
+                si = mid + 1;
             }
-            else if(arr[mid] > target){
-                end = mid - 1;
-            }
-            else start = mid + 1;
         }
-        return start;
+        return si;
     }
 }
