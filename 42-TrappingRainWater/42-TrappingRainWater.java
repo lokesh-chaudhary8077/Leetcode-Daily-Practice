3d0394d0c2d392fbc18ab5c7f63381d9b1a43c09
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2025-11-11 20:43:42 +0530

    File updated at 11/11/2025, 8:43:35 PM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,21 +1,40 @@
-// Last updated: 11/11/2025, 3:50:59 PM
+// Last updated: 11/11/2025, 8:43:35 PM
 class Solution {
-    public int trap(int[] arr) {
-        int n = arr.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
-        left[0] = arr[0];
-        for(int i = 1;i < n;i++){
-            left[i] = Math.max(left[i-1],arr[i]);
+    public List<Integer> spiralOrder(int[][] matrix) {
+      List<Integer> result = new ArrayList<>();
+      if(matrix == null || matrix.length == 0){
+        return result;
+      }
+      int minr = 0;
+      int minc = 0;
+      int maxr = matrix.length-1;
+      int maxc = matrix[0].length - 1;
+      int count = 0;
+      int total = matrix[0].length*matrix.length;
+      while(count < total){
+        for(int i = minc;i <= maxc && count < total;i++){
+            result.add(matrix[minr][i]);
+            count++;
         }
-        right[n-1] = arr[n-1];
-        for(int i = n-2;i >= 0;i--){
-            right[i] = Math.max(right[i+1],arr[i]);
+        minr++;
+
+        for(int i = minr;i <= maxr && count < total;i++){
+            result.add(matrix[i][maxc]);
+            count++;
         }
-        int area = 0;
-        for(int i = 0;i < n;i++){
-            area += Math.min(left[i],right[i]) - arr[i];
+        maxc--;
+        for(int i = maxc;i>= minc && count < total;i--){
+            result.add(matrix[maxr][i]);
+            count++;
         }
-        return area;
+        maxr--;
+        for(int i = maxr;i >= minr &&count < total;i--){
+            result.add(matrix[i][minc]);
+            count++;
+        }
+        minc++;
+      }
+      return result;
+      
     }
 }
