c226bf58fc31ca5b01059c8d6e6b573446000406
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2025-11-14 23:13:47 +0530

    Create 38-CountandSay

---
+++ 38-CountandSay/38-CountandSay.java
@@ -0,0 +1,18 @@
+// Last updated: 11/14/2025, 11:13:38 PM
+class Solution {
+    public String countAndSay(int n) {
+       if(n == 1) return "1";
+       StringBuilder s = new StringBuilder("");
+       String say = countAndSay(n-1);
+       for(int i = 0;i < say.length();i++){
+            char ch = say.charAt(i);
+            int count = 1;
+            while(i < say.length()-1 && say.charAt(i) == say.charAt(i+1)){
+                count++;
+                i++;
+            }
+            s.append(count).append(ch);
+       } 
+       return s.toString();
+    }
+}
