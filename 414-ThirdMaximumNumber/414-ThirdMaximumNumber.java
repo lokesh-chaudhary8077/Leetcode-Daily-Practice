f399539aafba4f990cb2c02b7701949e3eaedc0d
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2025-11-28 21:51:43 +0530

    File updated at 11/28/2025, 9:51:38 PM

--- 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.java
@@ -1,27 +1,27 @@
-// Last updated: 11/21/2025, 6:43:59 PM
-class Solution {
-    public int thirdMax(int[] nums) {
-        long v1 = Long.MIN_VALUE; 
-        long v2 = Long.MIN_VALUE;
-        long v3 = Long.MIN_VALUE;
-        for(int i = 0;i < nums.length;i++){
-            if(nums[i] == v1 || nums[i] == v2 || nums[i] == v3){
-                continue;
-            }
-            if(nums[i] > v1){
-                v3 = v2;
-                v2 = v1;
-                v1 = nums[i];
-            }
-            else if(nums[i] > v2){
-                v3 = v2;
-                v2 = nums[i];
-            }
-            else if(nums[i] > v3) {
-                v3 = nums[i];
-            }
-        }
-        if(v3 == Long.MIN_VALUE) return (int)v1;
-        else return (int)v3;
-    }
-}
+// Last updated: 11/28/2025, 9:51:38 PM
+1class Solution {
+2    public int thirdMax(int[] nums) {
+3        long v1 = Long.MIN_VALUE; 
+4        long v2 = Long.MIN_VALUE;
+5        long v3 = Long.MIN_VALUE;
+6        for(int i = 0;i < nums.length;i++){
+7            if(nums[i] == v1 || nums[i] == v2 || nums[i] == v3){
+8                continue;
+9            }
+10            if(nums[i] > v1){
+11                v3 = v2;
+12                v2 = v1;
+13                v1 = nums[i];
+14            }
+15            else if(nums[i] > v2){
+16                v3 = v2;
+17                v2 = nums[i];
+18            }
+19            else if(nums[i] > v3) {
+20                v3 = nums[i];
+21            }
+22        }
+23        if(v3 == Long.MIN_VALUE) return (int)v1;
+24        else return (int)v3;
+25    }
+26}
