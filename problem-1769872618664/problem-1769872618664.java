b4ffe4388c8c39e889617a25d208ae05a8ccd26c
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-01-31 20:47:11 +0530

    Create

---
+++ problem-1769872618664/problem-1769872618664.java
@@ -0,0 +1,15 @@
+// Last updated: 1/31/2026, 8:46:58 PM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int arr[] = {-1,-1};
+4        for(int i = 0;i < nums.length;i++){
+5            for(int j = i + 1;j < nums.length;j++){
+6                if(nums[i] + nums[j] == target){
+7                    arr[0] = i;
+8                    arr[1] = j;
+9                }
+10            }
+11        }
+12        return arr;
+13    }
+14}
