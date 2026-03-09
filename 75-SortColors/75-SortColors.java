05ffa7cb08a44ed4f927254c6e16a64d577e0c31
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-09 23:44:33 +0530

    File updated at 3/9/2026, 11:44:22 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,28 +1,26 @@
-// Last updated: 11/10/2025, 9:50:49 PM
-class Solution {
-    public void sortColors(int[] nums) {
-        int n = nums.length;
-       int left = 0;
-       int right = n -1; 
-       int iteration = 0;
-       while( iteration <= right){
-        if(nums[iteration] == 0){
-            swap(nums,iteration,left);
-            iteration++;
-            left++;
-        }else if(nums[iteration] == 2){
-            swap(nums,iteration,right);
-            right--;
-        }
-        else{
-            iteration++;
-        }
-       }
-    }
-    public static void swap(int nums[],int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    
-    }
-}
+// Last updated: 3/9/2026, 11:44:22 PM
+1class Solution {
+2    public void sortColors(int[] nums) {
+3        int si = 0;
+4        int mid  = 0;
+5        int ei = nums.length - 1;
+6        while(mid <= ei){
+7            if(nums[mid] == 0){
+8                int temp = nums[mid];
+9                nums[mid] = nums[si];
+10                nums[si] = temp;
+11                mid++;
+12                si++;
+13            }
+14            else if(nums[mid] == 1){
+15                mid++;
+16            }
+17            else{
+18                int temp = nums[mid];
+19                nums[mid] = nums[ei];
+20                nums[ei] = temp;
+21                ei--; 
+22            }
+23        }
+24    }
+25}
