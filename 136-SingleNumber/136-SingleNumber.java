82c056a8841bac06add249207dad06a11d209e23
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-09 20:50:32 +0530

    File updated at 3/9/2026, 8:50:20 PM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,10 +1,10 @@
-// Last updated: 11/10/2025, 8:24:41 PM
-class Solution {
-    public int singleNumber(int[] nums) {
-        int num = 0;
-       for(int i = 0;i < nums.length;i++){
-             num = num ^ nums[i];
-       } 
-       return num;
-    }
-}
+// Last updated: 3/9/2026, 8:50:20 PM
+1class Solution {
+2    public int singleNumber(int[] nums) {
+3        int ans = nums[0];
+4        for(int i = 1;i < nums.length;i++){
+5            ans = ans ^ nums[i];
+6        }
+7        return ans;
+8    }
+9}
