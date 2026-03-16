57845f08bdc73b2abd55340cd138bbe83265277f
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-16 14:39:08 +0530

    File updated at 3/16/2026, 2:38:52 PM

--- 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.java
+++ 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.java
@@ -1,25 +1,21 @@
-// Last updated: 3/16/2026, 2:33:37 PM
+// Last updated: 3/16/2026, 2:38:52 PM
 1class Solution {
 2    public int[] rearrangeArray(int[] nums) {
-3        int p[] = new int[nums.length/2];
-4        int n[] = new int[nums.length/2]; 
-5        int pidx = 0;
-6        int nidx = 0;
+3        int arr[] = new int[nums.length];
+4        
+5        int pi = 0;
+6        int ni = 1;
 7        for(int i = 0;i< nums.length;i++){
 8            if(nums[i] > 0){
-9                p[pidx++] = nums[i];
-10            }
-11            else{
-12                n[nidx++] = nums[i];
-13            }
-14        }
-15        int pi = 0;
-16        int ni = 0;
-17        for(int i = 0;i < nums.length;i=i+2){
-18            nums[i] = p[pi++];
-19            nums[i+1] = n[ni++];
-20
-21        }
-22        return nums;
-23    }
-24}
+9                arr[pi] = nums[i];
+10                pi +=2;
+11            }
+12            else{
+13                arr[ni] = nums[i];
+14                ni += 2;
+15            }
+16        }
+17        
+18        return arr;
+19    }
+20}
