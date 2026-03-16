ea9bb2bba22a2a3609016002b69436c1b527db5f
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-16 13:57:59 +0530

    File updated at 3/16/2026, 1:57:43 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,20 +1,18 @@
-// Last updated: 1/29/2026, 9:53:25 PM
+// Last updated: 3/16/2026, 1:57:43 PM
 1class Solution {
-2    public int[] rearrangeArray(int[] nums) {
-3        int n = nums.length;
-4        int p = 0;
-5        int ne = 1;
-6        int ans[] = new int[n];
-7        for(int i = 0;i < n;i++){
-8            if(nums[i] > 0){
-9                ans[p] = nums[i];
-10                p += 2;
-11            }
-12            else{
-13                ans[ne] = nums[i];
-14                ne += 2;
-15            }
-16        }
-17        return ans;
-18    }
-19}
+2    public int majorityElement(int[] nums) {
+3        int count = 0;
+4        int p = nums[0];
+5        for(int i = 1;i < nums.length;i++){
+6            if(p == nums[i]){
+7                count++;
+8            }
+9            else if(count == 0){
+10                p = nums[i];
+11            }else{
+12                count--;
+13            }
+14        }
+15        return p;
+16    }
+17}
