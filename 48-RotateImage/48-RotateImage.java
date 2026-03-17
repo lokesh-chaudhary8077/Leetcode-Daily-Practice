e787566f859d2a6a8d5837c0984bced665bfd6e6
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-17 23:34:27 +0530

    File updated at 3/17/2026, 11:34:10 PM

--- 48-RotateImage/48-RotateImage.java
+++ 48-RotateImage/48-RotateImage.java
@@ -1,24 +1,20 @@
-// Last updated: 1/30/2026, 10:09:48 PM
+// Last updated: 3/17/2026, 11:34:10 PM
 1class Solution {
 2    public void rotate(int[][] nums) {
-3        int n = nums.length;
-4        for (int i = 0; i < n; i++) {
-5            for (int j = i; j < n; j++) {
-6                if (i == j) {
-7                    continue;
-8                } else {
-9                    int temp = nums[i][j];
-10                    nums[i][j] = nums[j][i];
-11                    nums[j][i] = temp;
-12                }
-13            }
-14        }
-15        for (int i = 0; i < n; i++) {
-16            for (int j = 0; j < n/2; j++) {
-17                int temp = nums[i][j];
-18                nums[i][j] = nums[i][n-1-j];
-19                nums[i][n-1-j] = temp;
-20            }
-21        }
-22    }
-23}
+3      int n = nums.length;
+4      for(int i = 0;i < n;i++){
+5        for(int j = i;j < n;j++){
+6            int temp = nums[i][j];
+7            nums[i][j] = nums[j][i];
+8            nums[j][i] = temp;
+9        }
+10      }
+11      for(int i = 0;i < n;i++){
+12        for(int j = 0;j < n/2;j++){
+13            int temp = nums[i][n-1-j];
+14            nums[i][n-1-j] = nums[i][j];
+15            nums[i][j] = temp;
+16        }
+17      }
+18    }
+19}
