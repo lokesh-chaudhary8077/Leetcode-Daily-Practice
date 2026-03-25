62513aced6f42b00e8f16f8db359417e4796a786
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-25 23:13:27 +0530

    File updated at 3/25/2026, 11:13:18 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,13 +1,12 @@
-// Last updated: 3/9/2026, 8:27:23 PM
+// Last updated: 3/25/2026, 11:13:18 PM
 1class Solution {
 2    public int missingNumber(int[] nums) {
 3        int n = nums.length;
-4        int sum = 0;
-5        for(int i = 0;i < n;i++){
-6            sum += nums[i];
-7        }
-8        int total = n *( n + 1) / 2;
-9        int ans = total - sum;
-10        return ans;
-11    }
-12}
+4        int total = n * (n+1)/2;
+5        int sum = 0;
+6        for(int i = 0;i < n;i++){
+7            sum += nums[i];
+8        }
+9        return total-sum;
+10    }
+11}
