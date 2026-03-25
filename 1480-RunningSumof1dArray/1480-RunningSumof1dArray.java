0ba3c6f9344e50f6cc39ebf267bd07c616fe7dfa
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-25 22:50:01 +0530

    File updated at 3/25/2026, 10:49:53 PM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
@@ -1,16 +1,20 @@
-// Last updated: 3/25/2026, 9:48:20 PM
+// Last updated: 3/25/2026, 10:49:53 PM
 1class Solution {
-2    public int maximumWealth(int[][] accounts) {
-3      int sum = 0;
-4      int max = 0;
-5      for(int i = 0;i < accounts.length;i++){
-6        sum = 0;
-7        for(int j = 0;j < accounts[0].length;j++){
-8            sum += accounts[i][j];
-9        }
-10        max = Math.max(max,sum);
-11        
-12      }
-13      return max;  
-14    }
-15}
+2    public int pivotIndex(int[] nums) {
+3        int total = 0;
+4        int sum = 0;
+5        for(int i = 0;i < nums.length;i++){
+6            total += nums[i];
+7        }
+8        
+9        for(int i = 0;i < nums.length;i++){
+10            
+11            
+12
+13            if(total - sum - nums[i] == sum) return i;
+14            sum += nums[i];
+15 
+16        }
+17        return -1;
+18    }
+19}
