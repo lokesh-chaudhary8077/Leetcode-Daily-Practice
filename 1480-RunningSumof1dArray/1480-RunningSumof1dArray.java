a814edba41e1588d6fde612d877c1468464bcc81
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-25 22:54:52 +0530

    File updated at 3/25/2026, 10:54:43 PM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
@@ -1,20 +1,10 @@
-// Last updated: 3/25/2026, 10:49:53 PM
+// Last updated: 3/25/2026, 10:54:43 PM
 1class Solution {
-2    public int pivotIndex(int[] nums) {
-3        int total = 0;
-4        int sum = 0;
-5        for(int i = 0;i < nums.length;i++){
-6            total += nums[i];
-7        }
-8        
-9        for(int i = 0;i < nums.length;i++){
-10            
-11            
-12
-13            if(total - sum - nums[i] == sum) return i;
-14            sum += nums[i];
-15 
-16        }
-17        return -1;
-18    }
-19}
+2    public int[] buildArray(int[] nums) {
+3      int arr[] = new int[nums.length];
+4      for(int i = 0;i < nums.length;i++){
+5        arr[i] = nums[nums[i]];
+6      }  
+7      return arr;
+8    }
+9}
