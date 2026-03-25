252ae855e5ebbe0838416f4969edb4294e62d7f1
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-25 23:54:56 +0530

    File updated at 3/25/2026, 11:54:47 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,19 +1,13 @@
-// Last updated: 3/25/2026, 11:42:40 PM
+// Last updated: 3/25/2026, 11:54:47 PM
 1class Solution {
-2    public int findNumbers(int[] nums) {
-3        
-4        int even = 0;
-5        for(int i = 0;i < nums.length;i++){
-6            int count = 0;
-7            int n = nums[i];
-8            while(n  >0){
-9                n = n / 10;
-10                count++;
-11            }
-12            if(count % 2 == 0){
-13                even++;
-14            }
-15        }
-16        return even;
-17    }
-18}
+2    public int[] shuffle(int[] nums, int n) {
+3        int arr[]  = new int[2*n];
+4        int idx = 0;
+5        for(int i = 0;i < n;i++){
+6            arr[idx] = nums[i]; 
+7            arr[idx+1] = nums[i + n];
+8            idx = idx + 2;
+9        }
+10        return arr;
+11    }
+12}
