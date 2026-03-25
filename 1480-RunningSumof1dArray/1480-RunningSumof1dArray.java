5955a4c11277f10159755d4e3703917aba0db712
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-25 21:48:29 +0530

    File updated at 3/25/2026, 9:48:20 PM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
@@ -1,15 +1,16 @@
-// Last updated: 3/25/2026, 9:44:01 PM
+// Last updated: 3/25/2026, 9:48:20 PM
 1class Solution {
 2    public int maximumWealth(int[][] accounts) {
 3      int sum = 0;
 4      int max = 0;
 5      for(int i = 0;i < accounts.length;i++){
-6        for(int j = 0;j < accounts[0].length;j++){
-7            sum += accounts[i][j];
-8        }
-9        max = Math.max(max,sum);
-10        sum = 0;
-11      }
-12      return max;  
-13    }
-14}
+6        sum = 0;
+7        for(int j = 0;j < accounts[0].length;j++){
+8            sum += accounts[i][j];
+9        }
+10        max = Math.max(max,sum);
+11        
+12      }
+13      return max;  
+14    }
+15}
