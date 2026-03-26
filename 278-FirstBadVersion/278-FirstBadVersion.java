57841b8797e983ed737a4b390ecda60e1b88496a
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-26 22:42:07 +0530

    File updated at 3/26/2026, 10:41:58 PM

--- 278-FirstBadVersion/278-FirstBadVersion.java
+++ 278-FirstBadVersion/278-FirstBadVersion.java
@@ -1,28 +1,19 @@
-// Last updated: 3/26/2026, 9:59:38 PM
+// Last updated: 3/26/2026, 10:41:58 PM
 1class Solution {
-2    public int search(int[] nums, int target) {
-3       int si = 0;
-4       int ei = nums.length - 1;
-5       while(si <= ei){
-6          int mid = si + (ei - si) / 2;
-7          if(nums[mid] == target) return mid;
-8          if(nums[si] <= nums[mid]){
-9            if(nums[si] <= target && nums[mid] > target){
-10                ei = mid - 1;
+2    public int findMin(int[] nums) {
+3        int si = 0;
+4        int ei = nums.length - 1;
+5        while(si < ei){
+6             int mid = si + (ei - si) / 2;
+7            if(nums[mid] > nums[ei]){
+8                si = mid + 1;
+9            }else{
+10                ei = mid;
 11            }
-12            else{
-13                si = mid + 1;
-14            }
-15          }
-16          else{
-17            if(nums[ei] >= target && nums[mid] < target){
-18                si = mid + 1;
-19            }
-20            else {
-21                ei = mid - 1;
-22            }
-23          }
-24       } 
-25       return -1;
-26    }
-27}
+12            
+13        }
+14        return nums[si];
+15
+16
+17    }
+18}
