0b70ad25490326f478f9ed899d127a16508f3e1e
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-26 21:59:48 +0530

    File updated at 3/26/2026, 9:59:38 PM

--- 278-FirstBadVersion/278-FirstBadVersion.java
+++ 278-FirstBadVersion/278-FirstBadVersion.java
@@ -1,23 +1,28 @@
-// Last updated: 3/26/2026, 9:32:17 PM
-1/* The isBadVersion API is defined in the parent class VersionControl.
-2      boolean isBadVersion(int version); */
-3
-4public class Solution extends VersionControl {
-5    public int firstBadVersion(int n) {
-6        int si = 1;
-7        int ei = n;
-8        int ans = 0;
-9        while(si <= ei){
-10             int mid = si + (ei - si) / 2;
-11            if(isBadVersion(mid)){
-12                ei = mid - 1;
-13                ans = mid;
+// Last updated: 3/26/2026, 9:59:38 PM
+1class Solution {
+2    public int search(int[] nums, int target) {
+3       int si = 0;
+4       int ei = nums.length - 1;
+5       while(si <= ei){
+6          int mid = si + (ei - si) / 2;
+7          if(nums[mid] == target) return mid;
+8          if(nums[si] <= nums[mid]){
+9            if(nums[si] <= target && nums[mid] > target){
+10                ei = mid - 1;
+11            }
+12            else{
+13                si = mid + 1;
 14            }
-15            else {
-16                si = mid + 1;
-17            }
-18        }
-19        return ans;
-20        
-21    }
-22}
+15          }
+16          else{
+17            if(nums[ei] >= target && nums[mid] < target){
+18                si = mid + 1;
+19            }
+20            else {
+21                ei = mid - 1;
+22            }
+23          }
+24       } 
+25       return -1;
+26    }
+27}
