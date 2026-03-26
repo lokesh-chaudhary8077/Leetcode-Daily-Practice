b7ae443e70518a8cca4e0ae8e62bd0b701b92200
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-03-26 21:21:42 +0530

    File updated at 3/26/2026, 9:21:32 PM

--- 35-SearchInsertPosition/35-SearchInsertPosition.java
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -1,18 +1,22 @@
-// Last updated: 11/11/2025, 10:37:08 PM
-class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int si = 0;
-        int ei = nums.length - 1;
-        while(si <= ei){
-            int mid = (si + ei)/2;
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[mid] > target){
-                ei = mid - 1;
-            }else {
-                si = mid + 1;
-            }
-        }
-        return si;
-    }
-}
+// Last updated: 3/26/2026, 9:21:32 PM
+1class Solution {
+2    public int searchInsert(int[] nums, int target) {
+3        int si = 0;
+4        int ei = nums.length - 1;
+5        while(si <= ei){
+6            int mid = (si + ei) /2;
+7            if(nums[mid] == target){
+8
+9                return mid;
+10            }
+11            else if(nums[mid] < target){
+12                si = mid + 1;
+13            }
+14            else {
+15                ei = mid - 1;
+16            }
+17        
+18        }
+19        return si;
+20    }
+21}
