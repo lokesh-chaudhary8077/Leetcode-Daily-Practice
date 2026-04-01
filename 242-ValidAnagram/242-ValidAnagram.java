ef3fdefc23ec127a7586042b667f55013fb363e6
Lokesh chaudhary <lokeshchaudhary807760@gmail.com>
2026-04-01 11:51:55 +0530

    File updated at 4/1/2026, 11:51:51 AM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,30 +1,18 @@
-// Last updated: 8/5/2025, 12:15:26 PM
-import java.util.*;
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i = 0;i < s.length();i++){
-            char ch = s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-            }
-            else map.put(ch,1);
-        }
-        for(int i = 0;i < t.length();i++){
-            char ch = t.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)-1);
-                if(map.get(ch) < 0){
-                    return false;
-                }
-            }
-            else {
-            return false;
-            }
-        }
-        return true;
-    }
-}
+// Last updated: 4/1/2026, 11:51:51 AM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        HashMap<Character,Integer> map = new HashMap<>();
+4        for(int i = 0;i < s.length();i++){
+5            char ch = s.charAt(i);
+6            map.put(ch,map.getOrDefault(ch,0) + 1);
+7        }
+8        for(int i = 0;i < t.length();i++){
+9            char ch = t.charAt(i);
+10            if(!map.containsKey(ch)) return false;
+11            map.put(ch,map.get(ch) -1);
+12            if(map.get(ch) == 0) map.remove(ch);
+13
+14        }
+15        return map.isEmpty();
+16    }
+17}
